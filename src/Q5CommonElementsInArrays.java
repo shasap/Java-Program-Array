5433725b1771d5b43f1cdf457032872f716053d5
shasap <sapan.shah.qa@gmail.com>
2022-05-09 23:19:38 +0100

    Program for finding common element in array

---
+++ src/Q5CommonElementsInArrays.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Q5CommonElementsInArrays {
+    public static void main(String[] args) {
+        int arr1[] = new int[] {2,7,17,3,4,3};
+        int arr2[] = new int[] {2,5,7,6,4};
+
+        System.out.println("1st Array : "+ Arrays.toString(arr1));
+        System.out.println("2nd Array : "+ Arrays.toString(arr2));
+
+        for (int i = 0; i < arr1.length ; i++)
+        {
+            for (int j = 0; j < arr2.length ; j++)
+            {
+                if(arr1[i] == arr2[j])
+                {
+                    System.out.println("Common elements in array : "+arr1[i]);
+                }
+            }
+
+        }
+    }
+}
